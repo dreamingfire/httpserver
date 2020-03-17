b0d3dd0bfc8f354d78c8c75de0800a30373042d5
dreamingfire <18810912096@163.com>
2020-03-17 21:16:58 +0800

    init

---
+++ src/main/java/ml/dreamingfire/group/prod/httpserver/application/HttpServerApplication.java
@@ -0,0 +1,7 @@
+package ml.dreamingfire.group.prod.httpserver.application;
+
+public class HttpServerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
